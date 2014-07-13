5de4a76119e9480d9199f2e17ac615e09766905d
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2014-07-13 14:05:44 +0900

    Initial Commit

---
+++ ArduinoMini4WD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ArduinoMini4WD
+//
+//  Created by Sumi Koichiro on 2014/07/13.
+//  Copyright (c) 2014年 sumyapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ArduinoMini4WDTests/ArduinoMini4WDTests.m
@@ -0,0 +1,34 @@
+//
+//  ArduinoMini4WDTests.m
+//  ArduinoMini4WDTests
+//
+//  Created by Sumi Koichiro on 2014/07/13.
+//  Copyright (c) 2014年 sumyapp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ArduinoMini4WDTests : XCTestCase
+
+@end
+
+@implementation ArduinoMini4WDTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
